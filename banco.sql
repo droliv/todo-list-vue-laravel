0765daeee96af04e5b3a8fb102ddf57dfead5867
Daniele Reis <reis-daniele@hotmail.com>
2019-11-06 01:01:30 -0300

    alteração banco.sql

--- banco.sql
+++ banco.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE bancotasks;
 USE bancotasks;
 
+/** Optei por criar a tabela utilizando o migrate do laravel
 CREATE TABLE tasks(
 codigo int(10) AUTO_INCREMENT,
 titulo varchar(30) NOT NULL,
 descricao varchar(50),
+status varchar(10),
 PRIMARY KEY (codigo)
 );
+
+*/
