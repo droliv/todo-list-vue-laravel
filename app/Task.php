7b1a5a93255fcce82fd9ccef3a859b446cadf00b
Daniele Reis <droliver.dev@gmail.com>
2019-11-07 06:26:05 -0300

    function index

--- app/Task.php
+++ app/Task.php
@@ -4,14 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Task extends Model
 {
-    /**
+     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'title', 'description', 'status'
+        'title', 'description', 'status',
     ];
 }
